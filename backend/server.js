156d462e24a726132ee03692b5e9334f17c5c5b4
soniprem831 <premsoni8838@gmail.com>
2025-01-29 15:40:41 +0530

    Deploying Chattie

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,13 @@
-import express from "express"
-import dotenv from "dotenv"
+import path from "path";
+import express from "express";
+import dotenv from "dotenv";
+
 // const express=require('express');
 // const dotenv=require('dotenv');
 
-import authRoutes from "./routes/auth.routes.js"
-import messageRoutes from "./routes/message.routes.js"
-import userRoutes from "./routes/user.routes.js"
+import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
+import userRoutes from "./routes/user.routes.js";
 
 
 import connectToMongoDb from "./db/connectToMongoDb.js";
@@ -15,6 +17,8 @@ import { app, server } from "./socket/socket.js"
 // const app=express();
 const PORT=process.env.PORT||5000;
 
+const __dirname=path.resolve();
+
 dotenv.config();
 
 app.use(express.json()); //to parse the incoming requests with Json payloads (from req.body)
@@ -23,6 +27,13 @@ app.use(cookieParser());
 app.use("/api/auth",authRoutes)
 app.use("/api/messages",messageRoutes)
 app.use("/api/users",userRoutes)
+
+app.use(express.static(path.join(__dirname,"/frontend/dist")))
+
+app.get("*",(req,res)=>{
+    res.sendFile(path.join(__dirname,"frontend","dist","index.html"))
+});
+
 
 // app.get("/",(req,res)=>{
 //     console.log("Hello World!")
